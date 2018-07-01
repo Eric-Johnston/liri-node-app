8ef55550adfa8c5021554f258ef9b04bc82a8e25
Eric J <ejohnston092@gmail.com>
2018-07-01 16:43:31 -0400

    quick fix

--- liri.js
+++ liri.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+var keys = require("./keys.js");
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var cmd = process.argv
+var argument = ""
 
-for(i = 2; i >= cmd.length; i++){
-    cmd = cmd[i];
+for(i = 2; i < cmd.length; i++){
+    argument = argument + cmd[i];
 }

370bb4d5f81c3fea6b521517a0e12f4f8364c43d
Eric J <ejohnston092@gmail.com>
2018-07-02 20:51:22 -0400

    omdb search working

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log("--------------------------------------------")
+console.log("-----------------------------------------------");
 
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
--- liri.js
+++ liri.js
@@ -4,13 +4,14 @@ var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require("request");
 var keys = require("./keys.js");
-var fs = require("fs")
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var cmd = process.argv
 var argument = ""
+var txtFile = [];
 
 for(i = 2; i < cmd.length; i++){
     argument = argument + cmd[i];
@@ -23,13 +24,20 @@ for(i = 2; i < cmd.length; i++){
             spotifySearch();
             break;
         case "movie-this":
-            /*movie function*/
+            if(cmd[3] == null){
+                defaultMovie()
+            }
+            else{
+                movieSearch();
+            }
             break;
         case "do-what-it-says":
-            /*fs .txt read*/
-        }
+            textRead();
+    }
 
         function myTweets(){
+            console.log("Grabbing tweets...");
+            console.log("-----------------------------------------------");
             var params = {screen_name: "CheeseTony", count: 5}
             client.get("statuses/user_timeline", params, function(error, tweets, response){
     
@@ -37,7 +45,7 @@ for(i = 2; i < cmd.length; i++){
                     if(!error){
                         console.log(tweets[i].created_at)
                         console.log(tweets[i].text)
-                        console.log("--------------------------------------------")
+                        console.log("-----------------------------------------------");
                     }
                     else{
                         console.log(error)
@@ -47,18 +55,81 @@ for(i = 2; i < cmd.length; i++){
         }
 
         function spotifySearch(){
+            console.log("Finding song...");
+            console.log("-----------------------------------------------");
             var params = {type: "track", query: cmd[3]}
             spotify.search(params, function(err, data){
                 if(!err){
-                    console.log("Artist: " + data.tracks.items[0].artists[0].name)
-                    console.log("Album: " + data.tracks.items[0].album.name)
-                    console.log("Song: " + data.tracks.items[0].name)
-                    console.log("Preview Link: " + data.tracks.items[0].preview_url)
-                    console.log("--------------------------------------------")
+                    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                    console.log("Album: " + data.tracks.items[0].album.name);
+                    console.log("Song: " + data.tracks.items[0].name);
+                    console.log("Preview Link: " + data.tracks.items[0].preview_url);
+                    console.log("--------------------------------------------");
                 }
                 else{
-                    return console.log("Error occured: " + err);
+                    console.log("Error occured: " + err);
+                    console.log("-----------------------------------------------");
                 }
+            })
+        }
+
+        function defaultMovie(){
+            var queryUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&apikey=7ea4a7e4";
+            console.log("No parameters entered. Try movie-this 'movie'")
+            console.log("-----------------------------------------------");
+            request(queryUrl, function(error, response, body){
+                if(!error && response.statusCode == 200){
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Release Year: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                    console.log("Country: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors)
+                    console.log("-----------------------------------------------");
+                }
+                else{
+                    console.log("An error occured: " + error)
+                    console.log("-----------------------------------------------");
+                }
+            })
+             
+        }
+
+        function movieSearch(){
+            var queryUrl = "http://www.omdbapi.com/?t=" + cmd[3] + "&apikey=7ea4a7e4"
+            console.log("Finding movie...")
+            console.log("-----------------------------------------------");
+            request(queryUrl, function(error, response, body){
+                if(!error && response.statusCode == 200){
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Release Year: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                    console.log("Country: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors)
+                    console.log("-----------------------------------------------");
+                }
+                else{
+                    console.log("An error occured: " + error)
+                    console.log("-----------------------------------------------");
+                }
+            })
+        }
+
+        function textRead(){
+            fs.readFile("random.txt", "utf-8", function(err, data){
+                var obj = false;
+                if(obj){
+                    return console.log(err)
+                }
+                var output = data
+                console.log(output)
+                txtFile.push(output)
+                console.log(txtFile)
             })
         }
 }

07fe61a5af3104853f84798f1d709dd2afb0aa25
Eric J <ejohnston092@gmail.com>
2018-07-04 01:09:51 -0400

    appendFile working

--- liri.js
+++ liri.js
@@ -9,9 +9,8 @@ var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-var cmd = process.argv
-var argument = ""
-var txtFile = [];
+var cmd = process.argv;
+var argument = "";
 
 for(i = 2; i < cmd.length; i++){
     argument = argument + cmd[i];
@@ -19,20 +18,31 @@ for(i = 2; i < cmd.length; i++){
     switch(argument){
         case "my-tweets":
             myTweets();
+            logTxt();
             break;
         case "spotify-this-song":
+            if(cmd[3] == null){
+                defualtSong();
+                logTxt();
+            }
+            else{
             spotifySearch();
+            logTxt();
+            }
             break;
         case "movie-this":
             if(cmd[3] == null){
                 defaultMovie()
+                logTxt();
             }
             else{
                 movieSearch();
+                logTxt();
             }
             break;
         case "do-what-it-says":
             textRead();
+            logTxt();
     }
 
         function myTweets(){
@@ -46,10 +56,30 @@ for(i = 2; i < cmd.length; i++){
                         console.log(tweets[i].created_at)
                         console.log(tweets[i].text)
                         console.log("-----------------------------------------------");
+                        logTxt(tweets[i].created_at +"\n"+tweets[i].text)
                     }
                     else{
                         console.log(error)
                     }
+                }
+            })
+        }
+
+        function defualtSong(){
+            var songName = "The Sign Ace of Base";
+            var params = {type: "track", query: songName};
+            spotify.search(params, function(err, data){
+                if(!err){
+                    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                    console.log("Album: " + data.tracks.items[0].album.name);
+                    console.log("Song: " + data.tracks.items[0].name);
+                    console.log("Preview Link: " + data.tracks.items[0].preview_url);
+                    console.log("--------------------------------------------");
+                    logTxt("Artist: "+data.tracks.items[0].artists[0].name+"\n"+"Album: " + data.tracks.items[0].album.name +"\n"+"Song: " + data.tracks.items[0].name +"\n"+"Preview Link: " + data.tracks.items[0].preview_url)
+                }
+                else{
+                    console.log("Error occured: " + err);
+                    console.log("-----------------------------------------------");
                 }
             })
         }
@@ -66,6 +96,7 @@ for(i = 2; i < cmd.length; i++){
                     console.log("Song: " + data.tracks.items[0].name);
                     console.log("Preview Link: " + data.tracks.items[0].preview_url);
                     console.log("--------------------------------------------");
+                    logTxt("Artist: "+data.tracks.items[0].artists[0].name+"\n"+"Album: " + data.tracks.items[0].album.name +"\n"+"Song: " + data.tracks.items[0].name +"\n"+"Preview Link: " + data.tracks.items[0].preview_url)
                 }
                 else{
                     console.log("Error occured: " + err);
@@ -89,6 +120,8 @@ for(i = 2; i < cmd.length; i++){
                     console.log("Plot: " + JSON.parse(body).Plot);
                     console.log("Actors: " + JSON.parse(body).Actors)
                     console.log("-----------------------------------------------");
+                    logTxt("Title: " + JSON.parse(body).Title+"\n"+"Release Year: " + JSON.parse(body).Year+"\n"+"IMDB Rating: " + JSON.parse(body).Ratings[0].Value+"\n"+"Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value+"\n"+"Country: " + JSON.parse(body).Country+"\n"+"Language: " + JSON.parse(body).Language+"\n"+"Plot: " + JSON.parse(body).Plot+"\n"+"Actors: " + JSON.parse(body).Actors)
+
                 }
                 else{
                     console.log("An error occured: " + error)
@@ -113,6 +146,7 @@ for(i = 2; i < cmd.length; i++){
                     console.log("Plot: " + JSON.parse(body).Plot);
                     console.log("Actors: " + JSON.parse(body).Actors);
                     console.log("-----------------------------------------------");
+                    logTxt("Title: " + JSON.parse(body).Title+"\n"+"Release Year: " + JSON.parse(body).Year+"\n"+"IMDB Rating: " + JSON.parse(body).Ratings[0].Value+"\n"+"Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value+"\n"+"Country: " + JSON.parse(body).Country+"\n"+"Language: " + JSON.parse(body).Language+"\n"+"Plot: " + JSON.parse(body).Plot+"\n"+"Actors: " + JSON.parse(body).Actors)
                 }
                 else{
                     console.log("An error occured: " + error);
@@ -121,31 +155,41 @@ for(i = 2; i < cmd.length; i++){
             })
         }
 
+        var logInfo = "";
+        function logTxt(logInfo = ""){
+            fs.appendFile("log.txt", "\n"+ logInfo+ "\n-----------------------------------------------", "utf-8", function(err){
+                if(err){
+                    console.log("Error has occured: " + err)
+                }
+            })
+        }
+
 }
-            function textRead(){
-                fs.readFile("random.txt", "utf-8", function(err, data){
-                    console.log("Finding song...");
-                    console.log("-----------------------------------------------");
-                    if(!err){
-                        var output = data.split(",");
-                        var songName = (output[1])
-                        var params = {type: "track", query: songName}
-                        spotify.search(params, function(err, data){
-                            if(!err){
-                                console.log("Artist: " + data.tracks.items[0].artists[0].name);
-                                console.log("Album: " + data.tracks.items[0].album.name);
-                                console.log("Song: " + data.tracks.items[0].name);
-                                console.log("Preview Link: " + data.tracks.items[0].preview_url);
-                                console.log("--------------------------------------------");
-                            }
-                            else{
-                                console.log("Error occured: " + err);
-                                console.log("-----------------------------------------------");
-                            }
-                        })
-                    }
-                    else{
-                        return console.log(err);
-                    }
-                })
-            }
+        function textRead(){
+            fs.readFile("random.txt", "utf-8", function(err, data){
+                console.log("Finding song...");
+                console.log("-----------------------------------------------");
+                if(!err){
+                    var output = data.split(",");
+                    var songName = (output[1])
+                    var params = {type: "track", query: songName}
+                    spotify.search(params, function(err, data){
+                        if(!err){
+                            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                            console.log("Album: " + data.tracks.items[0].album.name);
+                            console.log("Song: " + data.tracks.items[0].name);
+                            console.log("Preview Link: " + data.tracks.items[0].preview_url);
+                            console.log("--------------------------------------------");
+                            logTxt("Artist: "+data.tracks.items[0].artists[0].name+"\n"+"Album: " + data.tracks.items[0].album.name +"\n"+"Song: " + data.tracks.items[0].name +"\n"+"Preview Link: " + data.tracks.items[0].preview_url)
+                        }
+                        else{
+                            console.log("Error occured: " + err);
+                            console.log("-----------------------------------------------");
+                        }
+                    })
+                }
+                else{
+                    return console.log(err);
+                }
+            })
+        }
